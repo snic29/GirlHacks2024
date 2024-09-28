676264939567cb713825c495a1da497d4b487844
snic29 <stephanie.nicolaescu@gmail.com>
2024-09-28 16:54:44 -0400

    fixed errors

--- parseposting.py
+++ parseposting.py
@@ -5,10 +5,10 @@ def call_openai_chat(prompt):
     client = AzureOpenAI(
         azure_endpoint = "https://oai-usnc-gpt01.openai.azure.com/",
         api_key = os.getenv("AZURE_OPENAI_API_KEY"),
-        api_version="2018-11-01"
+        api_version="2023-03-15-preview"
     )
     completion = client.chat.completions.create(
-        model = "gpt-3.5",
+        model = "gpt-35-turbo",
         messages = prompt,
         temperature = 0,
         top_p = 0.95,
@@ -16,12 +16,12 @@ def call_openai_chat(prompt):
         presence_penalty = 0,
         stop = None
     )
-    text = completion.choices[0].message.context
+    text = completion.choices[0].message.content
     return text
 
 def getDesiredTraits(posting):
     openaimsg = [{"role": "user", "content": '''
-                    Given this job posting, and the inferred company culture, extract the 10 most important skills/qualities an applicant should have. Return as comma separated values.'''}] 
+                    Given this job posting, and the inferred company culture, extract the 10 most important qualities an applicant should have. Return as comma separated values.'''}] 
     openaimsg.append({"role": "user", "content": "Job Posting: {}, AI:".format(posting)})
     desiredSkills = call_openai_chat(openaimsg)
     return desiredSkills
